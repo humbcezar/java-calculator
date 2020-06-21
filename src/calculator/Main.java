9bcbebb9b7f965f8423c981c9fb9a10d8fae0220
Humberto <skateforevis@hotmail.com>
2020-06-20 21:05:51 -0300

    adding calc layout
    Signed-off-by: Humberto <skateforevis@hotmail.com>

--- src/calculator/Main.java
+++ src/calculator/Main.java
@@ -14,9 +14,9 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        Parent root = FXMLLoader.load(getClass().getResource("calc.fxml"));
+        primaryStage.setTitle("Java Calc");
+        primaryStage.setScene(new Scene(root, 200, 300));
         primaryStage.show();
     }
 }
